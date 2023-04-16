de53ea046d4d365b0fe293c7ed2b6308a67d65fe
div-anh0901 <anh.dev.2002@gmail.com>
2023-04-16 14:50:57 +0700

    narbar

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Grid ,Container} from '@mui/material'
+import React from 'react'
+import { Navbar } from '../components/Nabar'
+
+export default function Home() {
+  return (
+    <Grid>
+        <Navbar/>
+    </Grid>
+  )
+}
